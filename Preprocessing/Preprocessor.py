ebc5ed01379f1af5dffe1dc4c659c5ce07fab205
emanueleantoniofaraone <emanueleant03@gmail.com>
2025-01-08 12:30:39 +0100

    Fixata la standardizzazione non funzionante a causa della presenza di valori non numerici nel dataset

--- Preprocessing/Preprocessor.py
+++ Preprocessing/Preprocessor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 
 class Preprocessor:
+    __class_column_name = 'classtype_v1'
     __data_path = 'input.csv'
     def __init__(self, data_path: str):
         self.__data_path = data_path
@@ -20,23 +21,21 @@ class Preprocessor:
 
     def data_cleanup(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.drop(columns=['Sample code number'])
+        df = df.apply(pd.to_numeric, errors='coerce')
         df = df.dropna()
         return df
 
     def data_standardization(self, df: pd.DataFrame) -> pd.DataFrame:
-        mean_value : float = 0.0
-        std_value : float = 0.0
-        for column in df.columns:
-            if df[column].name != 'Sample code number':
-                mean_value = df[column].mean()
-                std_value = df[column].std()
-                for row in column:
-                    row = (row - mean_value) / std_value
+        class_column = df[self.__class_column_name]
+        df.drop(columns=[self.__class_column_name])
+        standardized_df = (df - df.mean()) / df.std()
+        standardized_df[self.__class_column_name] = class_column
+        return standardized_df
 
     def preprocess(self) -> pd.DataFrame:
         df = self.load_dataset()
         df = self.data_cleanup(df)
         df = self.data_standardization(df)
-        print(df)
+        return df
 
 
