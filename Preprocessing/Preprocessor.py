324c0ba77d783327ec196d862703a26334c9a16d
emanueleantoniofaraone <emanueleant03@gmail.com>
2025-01-09 22:32:47 +0100

    Commentato gran parte del PreProcessing e cambiato il metodo preprocess che ora prende in input il datapath (prima questo dato era passato nel costruttore).

--- Preprocessing/Preprocessor.py
+++ Preprocessing/Preprocessor.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 
+"""
+This abstract class can be implemented by a class capable of preprocessing a dataset.
+It was created to give flexibility on how to preprocess data, the "implementing" class 
+just needs to expose a preprocess method as described here and to return the processed dataset,
+the internal workings of the implementation can be different without any problems to the code.
+"""
 class Preprocessor(ABC):
+    """
+    This abstract method can be implemented by a class capable of preprocessing a dataset.
+    It takes the data path as input and returns the processed dataset divided in feature and target.
+    """
     @abstractmethod
-    def preprocess(self) -> pd.DataFrame:
+    def preprocess(self, data_path: str) -> [pd.DataFrame, pd.DataFrame]:
         pass
--- Preprocessing/impl/PreprocessorImplementation.py
+++ Preprocessing/impl/PreprocessorImplementation.py
@@ -1,37 +1,64 @@
-from typing import Any
-
 import pandas as pd
 import sys
 
 from pandas import DataFrame
-
 from Preprocessing.Preprocessor import Preprocessor
 
-
+"""
+This class extends the Preprocessor class and implements it's preprocess method.
+It does so by first loading the dataset from the input data path, cleaning up the data,
+standardizing it and splitting it into features and target variable.
+It has two variables: @__class_column_name which is the name of the column that will be used as 
+the target variable, and a default data path in @__data_path.
+"""
 class PreprocessorImplementation(Preprocessor):
-    __class_column_name = 'classtype_v1'
-    __data_path = 'input.csv'
-    def __init__(self, data_path: str):
-        self.__data_path = data_path
+    __class_column_name = 'classtype_v1'        # name of the column that will be used as  the target variable
+    __data_path = 'input.csv'                   # default (placeholder) data path
 
+    """
+    Empty class constructor that creates a new class instance
+    """
+    def __init__(self):
+        pass
+
+    """
+    This method loads the dataset from the input data path and returns it as a Pandas DataFrame.
+    It also checks and handles error while reading the file, such as FileNotFoundError or a 
+    generic exception by returning an error message and quitting the execution.
+    """
     def load_dataset(self) -> pd.DataFrame:
-        print('Loading dataset...')
-        print('Reading data from ' + self.__data_path)
+        print('Loading dataset...')                         # logging
+        print('Reading data from ' + self.__data_path)      # logging
         try:
-            df = pd.read_csv(self.__data_path)
-        except FileNotFoundError:
-            sys.exit('Dataset not found.')
-        except Exception as e:
-            print(e)
-            sys.exit('Error loading dataset.')
-        return df
+            df = pd.read_csv(self.__data_path)              # dataset is read from file
+        except FileNotFoundError:                           # handles a FileNotFoundError
+            sys.exit('Dataset not found.')                  # logs the error and quits
+        except Exception as e:                              # handles every other exception
+            print(e)                                        # prints the exception
+            sys.exit('Error loading dataset.')              # logs the error and quits
+        return df                                           # if no errors have occurred returns the dataframe
 
+    """
+    This method cleans up the dataframe given to it as a parameter:
+    it first drops the sample code number column that is contained in the dataset per specification,
+    then tries to convert all values to numeric, if it encounters errors during this process ( for example 
+    if it finds a string and cannot convert it to numeric) it removes the row from the dataframe.
+    It also removes all na rows and finally returns the dataframe.
+    """
     def data_cleanup(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.drop(columns=['Sample code number'])
-        df = df.apply(pd.to_numeric, errors='coerce')
-        df = df.dropna()
-        return df
+        df = df.drop(columns=['Sample code number'])            # drops not useful 'Sample code number' column
+        df = df.apply(pd.to_numeric, errors='coerce')           # drops all non-numeric rows
+        df = df.dropna()                                        # drops the rows with NA values
+        return df                                               # returns the dataframe
 
+    """
+    This method standardizes the dataframe given to it as a parameter:
+    It first removes the target column (__class_column_name) saving it in a temporary variable. 
+    This is done to not modify this information which will be re-added after the dataframe is standardized.
+    Then proceeds to standardize all the data by subtracting the mean of that column and dividing by the standard 
+    deviation of said column.
+    The output will be the dataframe standardized with the class column added back in.
+    """
     def data_standardization(self, df: pd.DataFrame) -> pd.DataFrame:
         class_column = df[self.__class_column_name]
         df.drop(columns=[self.__class_column_name])
@@ -40,9 +67,10 @@ class PreprocessorImplementation(Preprocessor):
         return standardized_df
 
     def get_targets_and_features(self, df: pd.DataFrame) -> tuple[DataFrame, DataFrame]:
-        return df.columns[self.__class_column_name], df.drop(columns=[self.__class_column_name])
+        return df[self.__class_column_name], df.drop(columns=[self.__class_column_name])
 
-    def preprocess(self) -> tuple[DataFrame, DataFrame]:
+    def preprocess(self, data_path: str) -> tuple[DataFrame, DataFrame]:
+        self.__data_path = data_path
         df = self.load_dataset()
         df = self.data_cleanup(df)
         df = self.data_standardization(df)
