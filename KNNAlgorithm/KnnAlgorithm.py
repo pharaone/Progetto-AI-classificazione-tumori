adb48a55f3916251255c5299a67727eb9a598d5e
stegi <stefano.1806@gmail.com>
2025-01-12 18:42:48 +0100

    creato lo scheletro della classe KnnAlgorithm

---
+++ KNNAlgorithm/KnnAlgorithm.py
@@ -0,0 +1,8 @@
+class KnnAlgorithm:
+    def __init__(self, k, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.k = k
+
+    def __class__(self, x_test):
+        pass
