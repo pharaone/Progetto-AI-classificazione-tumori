0e87473def53e69423dd32bdf1bf6a87ac09ab84
emanueleantoniofaraone <emanueleant03@gmail.com>
2025-01-12 16:15:29 +0100

    Correzione errore: correzione da classe astratta a interfaccia per Preprocessor.py

--- Preprocessing/Preprocessor.py
+++ Preprocessing/Preprocessor.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 """
-This abstract class can be implemented by a class capable of preprocessing a dataset.
+This interface can be implemented by a class capable of preprocessing a dataset.
 It was created to give flexibility on how to preprocess data, the "implementing" class 
 just needs to expose a preprocess method as described here and to return the processed dataset,
 the internal workings of the implementation can be different without any problems to the code.
