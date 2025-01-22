7a439253d753b6ebdccd2af34e5f83a7e7a4c8a8
emanueleantoniofaraone <emanueleant03@gmail.com>
2025-01-22 22:47:16 +0100

    Commentata buona parte della classe evaluator

--- Evaluation/Evaluator.py
+++ Evaluation/Evaluator.py
@@ -6,7 +6,11 @@ import numpy as np
 
 from KNNAlgorithm.KnnAlgorithm import KnnAlgorithm
 
-
+"""
+This class contains all the code needed to evaluate and export the evaluation data of the model
+using different methods of validation:
+Holdout, K-fold cross validation and Stratified cross validation
+"""
 class Evaluator:
     __features : pd.DataFrame = None
     __targets : pd.Series = None
@@ -45,53 +49,66 @@ class Evaluator:
 
         self.save_metrics(self.calculate_metrics(y_test, y_pred))       # Calculate and return the evaluation metrics
 
+    """
+    This method divides the data into k (user inserted parameter) subsets for k-fold cross-validation.
+    It trains the KNN model on k-1 subsets and evaluates on the remaining one for k times
+    calculating metrics each time and averaging them across all folds.
+    """
     def k_fold_cross_validation(self, k_times: int, k_neighbors: int):
+        # divide the features and targets into k subsets
         features_subsets : [pd.DataFrame] = np.array_split(self.__features, k_times)
         targets_subsets : [pd.Series] = np.array_split(self.__targets, k_times)
 
-        metrics = []
+        metrics = []                                                            # created an empty metrics array
 
         for index in range(k_times):
+            # create a feature and a target set without the current index fold
             features_set = features_subsets.copy().pop(index)
             targets_set = targets_subsets.copy().pop(index)
 
-            knn = KnnAlgorithm(k_neighbors, features_set, targets_set)
-            y_prediction = knn.predict(features_subsets[index])
+            knn = KnnAlgorithm(k_neighbors, features_set, targets_set)          # creates an instance of the knn model
+            y_prediction = knn.predict(features_subsets[index])                 # runs the prediction
 
-            metrics.append(self.calculate_metrics(targets_set, y_prediction))
+            metrics.append(self.calculate_metrics(targets_set, y_prediction))   # calculates the requested metrics for this evaluation and appends them to the list
 
-        self.save_metrics_from_metrics_list(metrics)
+        self.save_metrics_from_metrics_list(metrics)                            # saves to file the requested metrics
 
+    """
+    This method performs the stratified k-fold (k user defined parameter) cross-validation.
+    It trains the KNN model on k-1 folds each with the same class distribution as the original set 
+    and evaluates on the remaining one calculating metrics each time and averaging them across all folds.
+    """
     def stratified_cross_validation(self, k_times: int, k_neighbors: int):
-        unified_features_and_targets = self.__features.copy()
-        unified_features_and_targets['targets'] = self.__targets.copy()
-        unified_features_and_targets.sort_values(by=['targets'], inplace=True)
+        unified_features_and_targets = self.__features.copy()                           # creates a copy of the features
+        unified_features_and_targets['targets'] = self.__targets.copy()                 # adds the target column to the copy
+        unified_features_and_targets.sort_values(by=['targets'], inplace=True)          # sorts everything by class
 
-        benign_dataset = unified_features_and_targets[unified_features_and_targets['targets'] == 2.0]
-        malign_dataset = unified_features_and_targets[unified_features_and_targets['targets'] == 4.0]
+        benign_dataset = unified_features_and_targets[unified_features_and_targets['targets'] == 2.0]       # extracts the benign dataset
+        malign_dataset = unified_features_and_targets[unified_features_and_targets['targets'] == 4.0]       # extracts the malign dataset
 
-        benign_targets = np.array_split(benign_dataset['targets'], k_times)
-        malign_targets = np.array_split(malign_dataset['targets'], k_times)
+        benign_targets = np.array_split(benign_dataset['targets'], k_times)             # splits the benign targets in k folds
+        malign_targets = np.array_split(malign_dataset['targets'], k_times)             # splits the malign targets in k folds
 
-        benign_dataset = benign_dataset.drop(columns=['targets'])
+        benign_dataset = benign_dataset.drop(columns=['targets'])                       # drops the targets column only used to split the benign and malingn
+                                                                                        # sets more easily from the dataset
         malign_dataset = malign_dataset.drop(columns=['targets'])
 
-        benign_subsets: [pd.DataFrame] = np.array_split(benign_dataset, k_times)
-        malign_subsets: [pd.DataFrame] = np.array_split(malign_dataset, k_times)
+        benign_subsets: [pd.DataFrame] = np.array_split(benign_dataset, k_times)        # creates the subsets of the dataset
+        malign_subsets: [pd.DataFrame] = np.array_split(malign_dataset, k_times)        # creates the subsets of the dataset
 
-        metrics = []
+        metrics = []                                                                    # created an empty metrics array
 
         for index in range(k_times):
-            features_set = pd.concat([benign_subsets.copy().pop(index), malign_subsets.copy().pop(index)])
-            targets_set = pd.concat([benign_targets.copy().pop(index), malign_targets.copy().pop(index)])
+            features_set = pd.concat([benign_subsets.copy().pop(index), malign_subsets.copy().pop(index)])          # unites benign and maling sets and removes the current index fold
+            targets_set = pd.concat([benign_targets.copy().pop(index), malign_targets.copy().pop(index)])           # unites benign and maling sets and removes the current index fold
 
-            knn = KnnAlgorithm(k_neighbors, features_set, targets_set)
-            y_prediction = knn.predict(pd.concat([benign_subsets[index], malign_subsets[index]]))
+            knn = KnnAlgorithm(k_neighbors, features_set, targets_set)                                  # creates an instance of the knn model
+            y_prediction = knn.predict(pd.concat([benign_subsets[index], malign_subsets[index]]))       # runs the prediction
 
-            metrics.append(self.calculate_metrics(targets_set, y_prediction))
+            metrics.append(self.calculate_metrics(targets_set, y_prediction))           # calculates the requested metrics for this evaluation and appends them to the list
 
 
-        self.save_metrics_from_metrics_list(metrics)
+        self.save_metrics_from_metrics_list(metrics)                                    # saves to file the requested metrics
 
     """
     Calculates the main evaluation metrics for the KNN model: 
